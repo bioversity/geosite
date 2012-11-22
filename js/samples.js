b1136b59019c55279e2d60a0dd62ea065b1c1874
Luca Matteis <lmatteis@gmail.com>
2012-11-22 15:30:42 +0100

    first samples part kinda done with implementation

--- js/samples.js
+++ js/samples.js
@@ -1,5 +1,7 @@
 samples = {
     instTable: '1g5rq1sLqXArypFIudfAXN1p5P0dMGfNmW7AwWD0',
+    coopTable: '1o-UvQHmOzovOgsYMlYpDD3kj0YDd8BBePhwcANE',
+    collTable: '1IevrMRdq9ZmQt10Oa8IaPJ2-wfheZkuXhzOvFTs',
     submit: function() {
         // do the Insititues part first if they contain data
         var instFilters = []
@@ -14,40 +16,124 @@ samples = {
             }
             
         })
-        var where = instFilters.join(' AND ')
-        var queryText = "SELECT INSTCODE " +
-                "FROM " + samples.instTable + " WHERE " + where;
+        var instQuery = "SELECT INSTCODE " +
+                "FROM " + samples.instTable + " WHERE " + instFilters.join(' AND ');
 
-        if(instFilters.length) { // institues fields were filled out
-            query.runQuery(queryText, function(data) {
-                var instCodes = []
-                for(var i in data.rows) {
-                    var instcode = data.rows[i][0]
-                    instCodes.push("'" + instcode + "'")
+        // cooperators
+        var coopFilters = []
+        $('div[groupName=cooperators] input').each(function() {
+            var $this = $(this)
+            var key, value, operator;
+            key = $this.attr('placeholder')
+            value = $this.val()
+            operator = '=' // defaulting to = (not using < or >)
+            if(value) {
+                coopFilters.push("'" + key + "' " + operator + " '" + value + "'")
+            }
+            
+        })
+        var coopQuery = "SELECT ID_COOPERATOR " +
+                "FROM " + samples.coopTable + " WHERE " + coopFilters.join(' AND ');
+
+        if(instFilters.length && coopFilters.length) { // both institutes and cooperators filled
+            query.runQuery(instQuery, function(data) { // first inst
+                var w = samples.buildWhereInst(data)
+                query.runQuery(coopQuery, function(data) { // then coop
+                    samples.buildWhereCoop(data, function(where) {
+                        if(w) {
+                            where += ' AND ' + w
+                        }
+                        var samplesWhere = samples.buildWhereSamples()
+                        if(samplesWhere) {
+                            where += ' AND ' + samplesWhere
+                        }
+                        if(where) {
+                            console.log(where)
+                            query.setWhere(where)
+                        }
+                    })
+                })
+            })
+        } else if(instFilters.length) { // only institues fields were filled
+            query.runQuery(instQuery, function(data) {
+                var where = samples.buildWhereInst(data)
+                var samplesWhere = samples.buildWhereSamples()
+                if(samplesWhere) {
+                    where += ' AND ' + samplesWhere
                 }
-
-
-                // add the other input filters
-                var filters = []
-                samples.addFilters(filters)
-                // using IN (OR isn't supported) instead of AND
-                var where = 'INSTCODE IN ('+instCodes.join(',')+')'
-                if(filters.length) {
-                    where += 'AND' + filters.join(' AND ')
+                if(where) {
+                    console.log(where)
+                    query.setWhere(where)
                 }
-                console.log(where)
-                query.setWhere(where)
-
+            })
+        } else if(coopFilters.length) { // only coop fields were filled
+            query.runQuery(coopQuery, function(data) {
+                samples.buildWhereCoop(data, function(where) {
+                    var samplesWhere = samples.buildWhereSamples()
+                    if(samplesWhere) {
+                        where += ' AND ' + samplesWhere
+                    }
+                    if(where) {
+                        console.log(where)
+                        query.setWhere(where)
+                    }
+                })
             })
         } else {
-            
+            var where = samples.buildWhereSamples()
+            if(where) {
+                console.log(where)
+                query.setWhere(where)
+            }
         }
 
-        return;
-
-
     },
-    addFilters: function(filters) {
+    buildWhereCoop: function(data, callback) {
+        var coopIds = []
+        for(var i in data.rows) {
+            var id_cooperator = data.rows[i][0]
+            coopIds.push("'" + id_cooperator + "'")
+        }
+        var q = 'SELECT ID_SUB_MISSION FROM '+samples.collTable+' WHERE ID_COOPERATOR IN (' + coopIds.join(',') + ')';
+        query.runQuery(q, function(data) {
+            var idSubMissions = []
+            for(var i in data.rows) {
+                var id_sub_mission = data.rows[i][0]
+                idSubMissions.push("'" + id_sub_mission + "'")
+            }
+
+            var where = ''
+            if(idSubMissions.length) { 
+                where = 'ID_SUB_MISSION IN (' + idSubMissions.join(',') + ')'
+            }
+
+            callback(where)
+        })
+    },
+    buildWhereInst: function(data) {
+        var instCodes = []
+        for(var i in data.rows) {
+            var instcode = data.rows[i][0]
+            instCodes.push("'" + instcode + "'")
+        }
+        var where = ''
+        if(instCodes.length) {
+            where = 'INSTCODE IN ('+instCodes.join(',')+')'
+        }
+
+        // add the other input filters
+        /*
+        var filters = []
+        samples.addFilters(filters)
+        if(filters.length) {
+            where += 'AND' + filters.join(' AND ')
+        }
+        */
+        // using IN (OR isn't supported) instead of AND
+        return where
+    },
+    buildWhereSamples: function() {
+        var filters = []
         // check the range lower and upper
         if(slide.filter) filters.push(slide.filter)
 
@@ -63,5 +149,7 @@ samples = {
                 filters.push("'" + key + "' " + operator + " '" + value + "'")
             }
         })
+
+        return filters.join(' AND ')
     }
 }
